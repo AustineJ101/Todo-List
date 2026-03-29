c40f28b6fe75965643627fb9bb868bf7ec0bd3c4
Austine Juma <austinejumah001@gmail.com>
2026-03-29 13:44:32 +0300

    feat: add data storage and retrieval functionality

--- src/store.js
+++ src/store.js
@@ -1,19 +1,54 @@
+import { Todo } from "./todo.js";
+
 const projects = {
-    default: [],
+    default: {id: "default", tasks: [], taskCount: 0}
 }
 
-function initializeAndRetrieveProjects(){
-    localStorage.setItem("projects", JSON.stringify(projects));
-    return JSON.parse(localStorage.getItem("projects"));
-}
+const activeProject = "default";
 
-function retrieveProjects(){
-    return JSON.parse(localStorage.getItem("projects"));
-}
 
 function getProjects(){
-    const projects = localStorage.getItem("projects")? retrieveProjects() : initializeAndRetrieveProjects();
-    return projects;
+    const data = localStorage.getItem("projects");
+    let parsedProjects;
+
+    if(data){
+        parsedProjects = JSON.parse(data);
+
+    }else{
+        localStorage.setItem("projects", JSON.stringify(projects)); 
+        parsedProjects = JSON.parse(localStorage.getItem("projects"));
+    }
+
+    //Recreate todo instances to preserve class methods 
+    for(const projectName in parsedProjects){
+        parsedProjects[projectName].tasks = parsedProjects[projectName].tasks.map(
+            item => new Todo(item)
+        )
+    }
+
+    return parsedProjects;
+
 }
 
-export { getProjects }
+function getActiveProject(){
+    const data = localStorage.getItem("activeProject");
+
+    if(data){
+         return JSON.parse(data);
+    }else{
+        localStorage.setItem("activeProject", JSON.stringify(activeProject));
+        return JSON.parse(localStorage.getItem("activeProject"));
+    }
+   
+}
+
+function activateProject(project){
+    localStorage.setItem("activeProject", JSON.stringify(project));
+}
+
+
+function updateProjects(projects){
+    localStorage.setItem("projects", JSON.stringify(projects))
+}
+
+export { getProjects, getActiveProject, activateProject, updateProjects }
