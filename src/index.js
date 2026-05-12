890029bbf0f146cddf2a3dec6d7cc9cfdfac2e5a
Austine Juma <austinejumah001@gmail.com>
2026-05-12 09:50:16 +0300

    set default project to local storage on page load

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./reset.css";
 import "./styles.css";
 import { renderProjects } from "./dom.js";
+import { projects } from "./store.js";
 
-
+localStorage.setItem("projects", JSON.stringify(projects)) //Send default project to local storage
 
 renderProjects();
 renderProjects();
